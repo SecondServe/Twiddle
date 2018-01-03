ba0a132eb77bf11009510e8e1766e8c776b37cef
Jeremiah Lewis <jeremiahlewis0710@gmail.com>
2018-01-03 13:03:50 -0500

    Added functioning touchListener to detect swipe direction. current direction is only up on right button
    Jeremiah

--- app/src/main/java/com/secondservestudios/twiddle/MainActivity.java
+++ app/src/main/java/com/secondservestudios/twiddle/MainActivity.java
@@ -1,13 +1,37 @@
 package com.secondservestudios.twiddle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button leftThumb;
+    Button rightThumb;
+    TextView timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        leftThumb = (Button) findViewById(R.id.leftThumb);
+        rightThumb = (Button) findViewById(R.id.rightThumb);
+        
+        
+        
+        rightThumb.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this){
+            public void onSwipeTop(){
+                Toast.makeText(MainActivity.this, "swiped up", Toast.LENGTH_SHORT).show();
+            }
+            
+        });
+
+
+
+
+
     }
+
 }
